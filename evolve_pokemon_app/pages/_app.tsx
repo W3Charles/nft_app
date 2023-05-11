4e59da14d0fc2fb9b6225f655c67a3fa10f4893d
W3Charles <www3charles@gmail.com>
2023-05-11 16:51:17 -0400

    fix: Error Invalid chain "sepolia"
    - Install thirdweb-dev/chains
    - Import Sepolia from thirdweb-dev/chains
    - Add Sepolia as value for activeChain const

--- evolve_pokemon_app/pages/_app.tsx
+++ evolve_pokemon_app/pages/_app.tsx
@@ -1,11 +1,13 @@
 import type { AppProps } from "next/app";
 import { ThirdwebProvider } from "@thirdweb-dev/react";
+import { Sepolia } from "@thirdweb-dev/chains";
 import "../styles/globals.css";
 
 // This is the chain your dApp will work on.
 // Change this to the chain your app is built for.
 // You can also import additional chains from `@thirdweb-dev/chains` and pass them directly.
-const activeChain = "ethereum";
+// const activeChain = "ethereum";
+const activeChain = Sepolia;
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
@@ -16,3 +18,5 @@ function MyApp({ Component, pageProps }: AppProps) {
 }
 
 export default MyApp;
+
+
