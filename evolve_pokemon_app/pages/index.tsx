b9f38a3f345f91eb0a02a22e7a1e6c410cc14dcc
W3Charles <www3charles@gmail.com>
2023-05-13 02:44:04 -0400

    feat: connect evolve contract to app front end

--- evolve_pokemon_app/pages/index.tsx
+++ evolve_pokemon_app/pages/index.tsx
@@ -1,53 +1,40 @@
-import { ConnectWallet } from "@thirdweb-dev/react";
+import { ConnectWallet, ThirdwebNftMedia, Web3Button, useAddress, useContract, useOwnedNFTs } from "@thirdweb-dev/react";
 import type { NextPage } from "next";
 import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
+  const { contract } = useContract(
+    "0xc30B9Bb2E6849Ab9bbFe5c6b253D8f0b8f48ADaA"
+  );
+
+  const address = useAddress();
+
+  const {data: nfts} = useOwnedNFTs(contract, address);
+
   return (
-    <div className={styles.container}>
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="http://thirdweb.com/">thirdweb</a>!
-        </h1>
-
-        <p className={styles.description}>
-          Get started by configuring your desired network in{" "}
-          <code className={styles.code}>pages/_app.tsx</code>, then modify the{" "}
-          <code className={styles.code}>pages/index.tsx</code> file!
-        </p>
-
-        <div className={styles.connect}>
-          <ConnectWallet />
+    <div>
+      <ConnectWallet theme="light" />
+      <br />
+      {nfts?.map((nft) => (
+        <div key={nft.metadata.id.toString()}>
+          <ThirdwebNftMedia metadata={nft.metadata}/>
+          {nft.metadata.name}
         </div>
-
-        <div className={styles.grid}>
-          <a href="https://portal.thirdweb.com/" className={styles.card}>
-            <h2>Portal &rarr;</h2>
-            <p>
-              Guides, references and resources that will help you build with
-              thirdweb.
-            </p>
-          </a>
-
-          <a href="https://thirdweb.com/dashboard" className={styles.card}>
-            <h2>Dashboard &rarr;</h2>
-            <p>
-              Deploy, configure and manage your smart contracts from the
-              dashboard.
-            </p>
-          </a>
-
-          <a
-            href="https://portal.thirdweb.com/templates"
-            className={styles.card}
-          >
-            <h2>Templates &rarr;</h2>
-            <p>
-              Discover and clone template projects showcasing thirdweb features.
-            </p>
-          </a>
-        </div>
-      </main>
+      ))}
+      <br />
+      <Web3Button
+        contractAddress={"0xc30B9Bb2E6849Ab9bbFe5c6b253D8f0b8f48ADaA"}
+        action={(contract) => contract.erc1155.claim(0, 1)}
+      >
+        Claim a Squirtle
+      </Web3Button>
+      <br />
+      <Web3Button
+        contractAddress={"0xc30B9Bb2E6849Ab9bbFe5c6b253D8f0b8f48ADaA"}
+        action={(contract) => contract.call("evolve")}
+      >
+        Evolve
+      </Web3Button>
     </div>
   );
 };
